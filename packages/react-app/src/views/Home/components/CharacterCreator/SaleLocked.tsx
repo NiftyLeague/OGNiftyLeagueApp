4ae7e1397852d77e40d9472e9d89c9b5872a1fb1
SnarfyMcSlappy <snarfy@nifty-league.com>
2021-09-23 15:16:25 -0700

    Updaet the launch message

--- packages/react-app/src/views/Home/components/CharacterCreator/SaleLocked.tsx
+++ packages/react-app/src/views/Home/components/CharacterCreator/SaleLocked.tsx
@@ -16,7 +16,11 @@ const SaleLocked = ({ totalSupply, saleLocked }: { totalSupply: number; saleLock
         message={totalSupply < 3 ? '⚠️ The sale has not started yet' : '✅ ALL DEGENS HAVE SOLD OUT!'}
         description={
           totalSupply < 3 ? (
-            <div>We will officially launch as soon as the 3rd NFT is minted!</div>
+            <div>
+              We are officially launching on <strong>Sept 24th</strong> as soon as
+              <br />
+              the <strong>3rd NFT</strong> is minted by the dev team!
+            </div>
           ) : (
             <div>Thank you all for the support!</div>
           )
