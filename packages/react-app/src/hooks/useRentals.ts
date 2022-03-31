49eb3a4f6698f0265d21ab3664346b9a1e3a0d74
heyradcode <betterthanbestest@gmail.com>
2022-03-31 08:14:33 -0700

    feat: added cache functionality

--- packages/react-app/src/hooks/useRentals.ts
+++ packages/react-app/src/hooks/useRentals.ts
@@ -4,6 +4,13 @@ export const RENTALS_URL = `${process.env.REACT_APP_AWS_BASED_API_URL || ''}prod
 
 type RENTALS_TYPE = { [key: string]: any };
 
+const CACHE_INTERVAL = 5 * 60 * 1000;
+
+let cache: {
+  timestamp: number | undefined;
+  data: RENTALS_TYPE | undefined;
+};
+
 const useRentals = (): [boolean, boolean, RENTALS_TYPE | undefined] => {
   const [loading, setLoading] = useState(true);
   const [rentals, setRentals] = useState<RENTALS_TYPE>();
@@ -16,18 +23,28 @@ const useRentals = (): [boolean, boolean, RENTALS_TYPE | undefined] => {
         return;
       }
 
-      const result = await fetch(RENTALS_URL, {
-        headers: { authorizationToken: authToken },
-      })
-        .then(res => {
-          if (res.status === 404) setError(true);
-          setLoading(false);
-          return res.json() as RENTALS_TYPE;
+      if (cache?.timestamp && new Date().getTime() - cache.timestamp < CACHE_INTERVAL) {
+        setRentals(cache.data);
+      } else {
+        const result = await fetch(RENTALS_URL, {
+          headers: { authorizationToken: authToken },
         })
-        .catch(() => {
-          setError(true);
-        });
-      if (result) setRentals(result);
+          .then(res => {
+            if (res.status === 404) setError(true);
+            setLoading(false);
+            return res.json() as RENTALS_TYPE;
+          })
+          .catch(() => {
+            setError(true);
+          });
+        if (result) {
+          setRentals(result);
+          cache = {
+            timestamp: new Date().getTime(),
+            data: result,
+          };
+        }
+      }
     }
     void resolveRentals();
   }, [authToken]);
